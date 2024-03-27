262e4c3881490fb2cf297c2bc8cbcbe10a94d7ec
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-03-27 10:39:17 +0700

    b7.2

--- buoi7/main.js
+++ buoi7/main.js
@@ -9,5 +9,17 @@ function child($scope,$rootScope){
     $rootScope.textGlobal ="Biến toàn cục nhưng được khai báo trong con";
 }   
 
+function myFunction($scope){
+    $scope.data ={
+        name: "chinhpd5",
+        gender: false,
+        mark: 4,
+        khoa: "UDPM",
+        hobbies: ["đá bóng","nhảy dây","đá cầu"]
+    }
+}
+
+
+app.controller("myControler",myFunction);
 app.controller("parent",parent);
 app.controller("child",child)
